0cfbbf5cc700c67f02bf23e4b0dda01ef325c931
Gnutt Halvordsson <gnutt@halvordsson.se>
2012-12-29 17:52:53 +0100

    List of issues now working

--- include/header.php
+++ include/header.php
@@ -16,7 +16,7 @@
 		echo "<a href=\"login.php\">Login</a>";
 	} else {
 		echo "<a href=\"issue.php?id=new\">[+] New Issue</a> | ";
-		echo "<a href=\"listissues.php\">View Issues</a> | ";
+		echo "<a href=\"listissues.php\">List Issues</a> | ";
 		echo $_SESSION['EMAIL'];
 		echo " | <a href=\"logout.php\">Logout</a>";
 	}
--- listissues.php
+++ listissues.php
@@ -1,9 +1,29 @@
 <?php include("include/header.php"); ?>
 		<h1>List issues</h1>
 <?php
-	$result = $db->query("SELECT * FROM issues");
+	$result = $db->query("SELECT issues._ROWID_ as issue, issues.title as title, issues.assigned as assigned, issues.updated as updated FROM issues");
+	echo "<table>\n";
+	echo "<tr>\n";
+	echo "<th>Issue #1</th>\n";
+	echo "<th>Title</th>\n";
+	echo "<th>Assigned to</th>\n";
+	echo "<th>Last updated</th>\n";
+	echo "</tr>\n";
 	while($row = $result->fetchArray(SQLITE3_ASSOC)) {
-		print_r($row);
+		echo "<tr>\n";
+		echo "<td>$row[issue]</td>\n";
+		echo "<td>$row[title]</td>\n";
+		echo "<td>";
+		if($row['assigned'] == -1) {
+			echo "Unassigned";
+		} else {
+			$username = $db->querySingle("SELECT email FROM users WHERE _ROWID_=$row[assigned]");
+			echo $username;
+		}
+		echo "</td>";
+		echo "<td>" . strftime("%Y-%m-%d %H:%I:%S", $row['updated']) . "</td>\n";
+		echo "</tr>\n";
 	}
+	echo "</table>\n";
 ?>
 <?php include("include/footer.php"); ?>
