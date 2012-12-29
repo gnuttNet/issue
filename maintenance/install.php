2d191527c2634206e61dfd1e9e63413be404bf97
Gnutt Halvordsson <gnutt@halvordsson.se>
2012-12-29 16:14:23 +0100

    Updating install to use exrec rather than query

--- maintenance/install.php
+++ maintenance/install.php
@@ -1,9 +1,9 @@
 <?php
 	mkdir("db");
 	$db = new SQLite3("db/tracker.sqlite");
-	$db->query("CREATE TABLE users(email text unique, realname text, password text, salt text, admin integer);");
-	$db->query("INSERT INTO users(email, realname, password, salt, admin) values(\"admin@localhost\",\"Dummy Administrator user\", \"" . sha1("adminsalt") . "\", \"salt\", 1)");
+	$db->exec("CREATE TABLE users(email text unique, realname text, password text, salt text, admin integer);");
+	$db->exec("INSERT INTO users(email, realname, password, salt, admin) values(\"admin@localhost\",\"Dummy Administrator user\", \"" . sha1("adminsalt") . "\", \"salt\", 1)");
 	echo "Adding Administrator user with email: admin@localhost, password: admin\n";
-	$db->query("CREATE TABLE issues(title text, description text, owner integer, assigned integer, createdate integer, updated integer");
+	$db->exec("CREATE TABLE issues(title text, description text, owner integer, assigned integer, createdate integer, updated integer");
 	$db->close();
 ?>
