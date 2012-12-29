eef7b9743ab32a3c34596a6e09efe3805b3f3336
Gnutt Halvordsson <gnutt@halvordsson.se>
2012-12-29 15:35:22 +0100

    Links in header for login/logout

--- include/header.php
+++ include/header.php
@@ -6,3 +6,14 @@
 		<link rel="StyleSheet" href="include/styelsheet.css" type="text/css" />
 	</head>
 	<body>
+	<div id="header">
+<?php
+	if(!isset($_SESSION['UID'])) {
+		echo "<a href=\"login.php\">Login</a>";
+	} else {
+		echo "<a href=\"newissue.php\">[+] New Issue</a> | ";
+		echo $_SESSION['EMAIL'];
+		echo "| <a href=\"logout.php\">Logout</a>";
+	}
+?>
+	</div>
