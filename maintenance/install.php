e415d00702807207dc290b55339b0eb442ecf375
Gnutt Halvordsson <gnutt@halvordsson.se>
2012-12-29 14:06:20 +0100

    Install.php now adds default user.

--- maintenance/install.php
+++ maintenance/install.php
@@ -1,5 +1,8 @@
 <?php
-	$db = new SQLite3(../db/users.sqlite, SQLITE3_OPEN_CREATE);
-
+	mkdir("db");
+	$db = new SQLite3("db/users.sqlite");
+	$db->query("CREATE TABLE users(email text unique, realname text, password text, salt text, admin integer);");
+	$db->query("INSERT INTO users(email, realname, password, salt, admin) values(\"admin@localhost\",\"Dummy Administrator user\", \"" . sha1("adminsalt") . "\", \"salt\", 1)");
+	echo "Adding Administrator user with email: admin@localhost, password: admin\n";
 	$db->close();
 ?>
