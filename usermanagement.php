79d9fcdb3cd6f61c71524ddaa008feedcaaedb69
MarkusRannare <mackyman@gmail.com>
2012-12-30 18:04:59 +0100

    Beginning of usermanagement page

--- usermanagement.php
+++ usermanagement.php
@@ -1,2 +1,12 @@
 <?php
+include("include/header.php");
+$result = $db->query("SELECT email,realname,admin FROM users");
+$users = $result->fetchArray(SQLITE3_ASSOC);
+
+
+print_r($users);
+?>
+
+<?php
+include("include/footer.php");
 ?>
