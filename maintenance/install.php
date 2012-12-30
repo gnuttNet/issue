956c7387f09ee4b1cd9a8cf8697b4f8811d3aa8a
Gnutt Halvordsson <gnutt@halvordsson.se>
2012-12-30 14:29:32 +0100

    Resolve issue #30

--- maintenance/install.php
+++ maintenance/install.php
@@ -10,7 +10,7 @@
 	$db->exec("CREATE TABLE users(email text unique, realname text, password text, salt text, admin integer);");
 	$db->exec("INSERT INTO users(email, realname, password, salt, admin) values(\"admin@localhost\",\"Dummy Administrator user\", \"" . sha1($randomPassword.$randomSalt) . "\", \"".$randomSalt."\", 1)");
 	echo "Adding Administrator user with email: admin@localhost, password: ".$randomPassword."\n";
-	$db->exec("CREATE TABLE issues(title text, description text, owner integer, assigned integer, createdate integer, updated integer)");
+	$db->exec("CREATE TABLE issues(title text, description text, owner integer, assigned integer, createdate integer, updated integer, status integer)");
 	$db->close();
 
 	chgrp("db/tracker.sqlite", "www-data");
